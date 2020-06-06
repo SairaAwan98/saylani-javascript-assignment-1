2fed6840ef159cdf108281409744aa980490a479
SairaAwan98 <64429895+SairaAwan98@users.noreply.github.com>
2020-06-06 22:47:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+// =================== chapter 1 alerts ==================
+
+// task # 01 
+alert(" hey users ! Welcome to my website TechZone");
+
+//task # 02
+alert("Error! Please enter a valid password");
+
+//task # 03
+alert("Welcome to JS land.... \n Happy Coding!");
+
+//task # 04
+alert("Welcome to Js land");
+alert("happy coding!");
+
+//task # 05
+alert("hello... I can rus JS through my web browser's console");
+
+// ============================ chapter 2 Variables =====================
+//task 1
+var username;
+//task 2
+var myName = "Saira Awan";
+//task 3
+var message = "Hello World";
+alert(message);
+//task 4
+var name= " Saira";
+var age= " 21";
+var certification = " Wordpress developer";
+alert(name);
+alert(age);
+alert(certification);
+//task 5
+var meme ="PIZZA";
+alert(meme);
+alert(meme.slice(0,meme.length-1));
+alert(meme.slice(0,meme.length-2));
+alert(meme.slice(0,meme.length-3));
+alert(meme.slice(0,meme.length-4));
+//task 6
+var email ="sairaawan2396@yahoo.com";
+alert("my email is "+email);
+//task 7
+var book="A Smarter way to learn javascript";
+alert("I am trying to learn from the book "+book);
+// task 9
+var sym= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(sym);
+
+
+// ============================ chapter 3 Variables for numbers =============================
+// task 1
+var age= 21;
+alert("I am "+age+" years old");
+// task 2
+var count;
+count=1;
+alert("you have visited the site "+count+" times");
+
+
+// ============================ chapter 4 Variables legal and illegal=============================
+// task 1
+var name,age,height;
+//task 2
+               //legal
+var age, fullName, _height ,school1 ,$gender;
+        // illegal
+
+var 2age;
+var full name;
+var alert;
+var "name";
+var gender();
+
+
+
